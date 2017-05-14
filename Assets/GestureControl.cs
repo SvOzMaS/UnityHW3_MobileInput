eed2748afb7cbc05913653aea33858573262b148
benzngf <b03902053@ntu.edu.tw>
2017-05-14 11:19:27 +0800

    refine GEsture Control

--- Assets/GestureControl.cs
+++ Assets/GestureControl.cs
@@ -5,6 +5,7 @@ using TouchScript.Hit;
 using DG.Tweening;
 
 public class GestureControl : MonoBehaviour {
+
 	public ScreenTransformGesture transformGesture;
 	public TapGesture singleTap;
 	public TapGesture doubleTap;
@@ -17,48 +18,48 @@ public class GestureControl : MonoBehaviour {
 	public Camera cam;
 	public LayerMask msk;
 
+	//Shake Stuff
 	private float accelerometerUpdateInterval = 1.0f / 60.0f;
-	// The greater the value of LowPassKernelWidthInSeconds, the slower the
-	// filtered value will converge towards current input sample (and vice versa).
 	private float lowPassKernelWidthInSeconds = 1.0f;
-	// This next parameter is initialized to 2.0 per Apple's recommendation,
-	// or at least according to Brady! ;)
 	private float shakeDetectionThreshold = 2.0f;
-
 	private float lowPassFilterFactor;
 	private float actionTime;
 	Vector3 lowPassValue;
+
+
 	void Update()
 	{
+		//Shake Stuff
 		Vector3 acceleration = Input.acceleration;
 		lowPassValue = Vector3.Lerp(lowPassValue, acceleration, lowPassFilterFactor);
 		Vector3 deltaAcceleration = acceleration - lowPassValue;
 		actionTime -= Time.deltaTime;
 		if (deltaAcceleration.sqrMagnitude >= shakeDetectionThreshold && actionTime <0.0f)
 		{
-			// Perform your "shaking actions" here. If necessary, add suitable
-			// guards in the if check above to avoid redundant handling during
-			// the same shake (e.g. a minimum refractory period).
 			actionTime = 1.2f;
 			animator.SetTrigger ("Shake");
 		}
 	}
-	// Use this for initialization
+	
 	void Start () {
+		//shake stuff
 		actionTime = 0.0f;
 		shakeDetectionThreshold *= shakeDetectionThreshold;
 		lowPassValue = Input.acceleration;
-		lowPassFilterFactor = accelerometerUpdateInterval /
-			lowPassKernelWidthInSeconds;
+		lowPassFilterFactor = accelerometerUpdateInterval / lowPassKernelWidthInSeconds;
+
 
 		flickable = true;
 		characters = GameObject.FindGameObjectsWithTag ("Chars");
+
+		//initial first character
 		animator = characters [0].GetComponent<Animator> ();
 		for(int i = 1; i < characters.Length; i++){
 			characters [i].SetActive (false);
 		}
-		Debug.Log (characters.Length);
 
+
+		//TouchScript
 		transformGesture.Transformed += (object sender, System.EventArgs e) => 
 		{
 			this.transform.Rotate(new Vector3(0,1,0),transformGesture.DeltaPosition[0]*1.0f);
@@ -69,8 +70,9 @@ public class GestureControl : MonoBehaviour {
 		{
 			TouchHit hit;
 			singleTap.GetTargetHitResult(out hit);
-
+			//RayCast
 			if(Physics.Raycast(new Ray(cam.transform.position,hit.Point-cam.transform.position),float.MaxValue,msk)){
+				//do stuff if tapped on the character
 				animator.SetTrigger("SingleTap");
 			}
 
@@ -80,8 +82,9 @@ public class GestureControl : MonoBehaviour {
 		{
 			TouchHit hit;
 			doubleTap.GetTargetHitResult(out hit);
-
+			//RayCast
 			if(Physics.Raycast(new Ray(cam.transform.position,hit.Point-cam.transform.position),float.MaxValue,msk)){
+				//do stuff if tapped on the character
 				animator.SetTrigger("DoubleTap");
 			}
 		};
@@ -94,21 +97,22 @@ public class GestureControl : MonoBehaviour {
 				bool toLeft = (gesture.ScreenFlickVector.x < 0);
 				this.transform.rotation = new Quaternion(0,0,0,0);
 				if(toLeft){
-					Debug.Log("to left");
+					//character move to left
 					changeChar = (currentChar+1)%characters.Length;
-					//animator.SetTrigger("Run");
 					characters[currentChar].transform.DOLocalMoveX(-2f, 1f).SetEase(Ease.InOutSine);
 					characters[changeChar].transform.localPosition = new Vector3(4f,0f,0f);
 					characters[changeChar].SetActive(true);
 					characters[changeChar].transform.DOLocalMoveX(0f, 1f).SetEase(Ease.InOutSine);
+
 					Invoke("AfterFlick", 1f);
 				}else{
-					Debug.Log("to right");
+					//character move to right
 					changeChar = (currentChar-1+characters.Length)%characters.Length;
 					characters[currentChar].transform.DOLocalMoveX(2f, 1f).SetEase(Ease.InOutSine);
 					characters[changeChar].transform.localPosition = new Vector3(-4f,0f,0f);
 					characters[changeChar].SetActive(true);
 					characters[changeChar].transform.DOLocalMoveX(0f, 1f).SetEase(Ease.InOutSine);
+
 					Invoke("AfterFlick", 1f);
 				}
 			}
@@ -116,6 +120,7 @@ public class GestureControl : MonoBehaviour {
 	}
 	private void AfterFlick()
 	{
+		//done transition (1sec), set previous character to inactive
 		characters[currentChar].SetActive(false);
 		currentChar = changeChar;
 		flickable = true;
